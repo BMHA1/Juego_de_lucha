9eb4688dea72b977fc8a83405ab31ec53ebae655
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-10-14 22:46:54 +0200

    proceso de seleccion de personajes en funcionamiento

--- js/index.js
+++ js/index.js
@@ -10,9 +10,9 @@ class Escenario {
 }
 
 class Avatar {
-    constructor(nombre, poder) {
+    constructor(nombre, fuerza) {
         this.nombre = nombre;
-        this.poder = poder
+        this.fuerza = fuerza
     }
 
 }
@@ -20,7 +20,7 @@ class Jugador {
 
     constructor(avatar) {
         this.vida = 100
-        this.avatar=avatar
+        this.avatar = avatar
     }
 }
 // ganar(vida) {
@@ -42,17 +42,62 @@ console.log(avatar3)
 let avatar4 = new Avatar("Luke Skywalker", "450")
 console.log(avatar4)
 
-let jugador1= new Jugador()
+let jugador1 = new Jugador()
 console.log(jugador1)
-let jugador2= new Jugador()
-console.log(jugador1)
+let jugador2 = new Jugador()
+console.log(jugador2)
+let escenario1 = new Escenario()
 
-let escenario1= new Escenario()
+// PROCESO DE SELECCION 
+const avatarDarthVader = () => {
+    // alert("hola") //entra en función 
 
+    if (jugador1.avatar == undefined) { //si jugador1.avatar está vacío inserta a avatar1 
+        let result = jugador1.avatar = avatar1
+        console.log(result) //prueba exitosa
+    } else if (jugador1.avatar !== undefined && jugador2.avatar == undefined) { //si jugador1.avatar ya está lleno, inserta a avatar1 a jugador2 
+        jugador2.avatar = avatar1
+    } else if (jugador1.avatar == undefined && jugador2.avatar == undefined) {
 
-seleccionaJugador = () => {
-
-
-
-
+        alert("ERROR!!")
+    }
 }
+
+const avatarYoda = () => {
+    // alert("hola") //entra en función 
+
+    if (jugador1.avatar == undefined) { //si jugador1.avatar está vacío inserta a avatar2
+        let result = jugador1.avatar = avatar2
+        console.log(result) //prueba exitosa
+    } else if (jugador1.avatar !== undefined && jugador2.avatar == undefined) { //si jugador1.avatar ya está lleno, inserta a avatar1 a jugador2 
+        jugador2.avatar = avatar2
+    } else if (jugador1.avatar !== undefined && jugador2.avatar !== undefined) {
+        alert("ERROR!!")
+    }
+}
+
+const avatarDarthHall = () => {
+    // alert("hola") //entra en función 
+    if (jugador1.avatar == undefined) { //si jugador1.avatar está vacío inserta a avatar2
+        let result = jugador1.avatar = avatar3
+        console.log(result) //prueba exitosa
+    } else if (jugador1.avatar !== undefined && jugador2.avatar == undefined) { //si jugador1.avatar ya está lleno, inserta a avatar a jugador2 
+        jugador2.avatar = avatar3
+    } else if (jugador1.avatar !== undefined && jugador2.avatar !== undefined) {
+        alert("ERROR!!")
+    }
+}
+const avatarLukeSkywalker = () => {
+    alert("hola") //entra en función
+    if (jugador1.avatar == undefined) { //si jugador1.avatar está vacío inserta a avatar2
+        let result = jugador1.avatar = avatar4
+        console.log(result) //prueba exitosa
+    } else if (jugador1.avatar !== undefined && jugador2.avatar == undefined) { //si jugador1.avatar ya está lleno, inserta a avatar1 a jugador2 
+        jugador2.avatar = avatar4
+    } else if (jugador1.avatar !== undefined && jugador2.avatar !== undefined) {
+        alert("ERROR!!")
+    }
+}
+
+
+// PROCESO DE SELECCION 
