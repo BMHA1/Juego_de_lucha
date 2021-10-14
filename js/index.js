48bac77a31e7e203090964d391469f4ebd382818
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-10-14 20:54:12 +0200

    prueba-instanciando clases

--- js/index.js
+++ js/index.js
@@ -1,40 +1,58 @@
+class Escenario {
+    constructor(seleccion1, seleccion2, ganador) {
 
-
-let nombres={
-    
-    nombre:"juan",
-    apellido:"perez"
-}
-
-console.log(nombres.nombre)
-
-console.log(nombres.hasOwnProperty("nombre"))
-
-class Escenario{
-    constructor(jugador1, jugador2, ganador){
-
-        this.jugador1=jugador1
-        this.jugador2=jugador2
-        this.ganador=null
+        // this.jugador1 = seleccion1
+        // this.jugador2 = seleccion2
+        // this.ganador = null
     }
 
-        ataque(atacante){
-            if(atacante==1){
-                this.jugador2.vida-=1
-            }
-        }
-    
+
+}
+
+class Avatar {
+    constructor(nombre, poder) {
+        this.nombre = nombre;
+        this.poder = poder
+    }
+
 }
 class Jugador {
-        constructor(nombre){
 
-            this.nombre = nombre;
-            this.vida=100;
+    constructor(avatar) {
+        this.vida = 100
+        this.avatar=avatar
+    }
+}
+// ganar(vida) {
+//     if (vida.Avatar1.vida == 0) {
+//         alert(`GANASTE+ ${avatar2}`)
+//     } else if () {
 
-        }
+
+//     }
+// }
+
+
+let avatar1 = new Avatar("Darth Vader", "200")
+console.log(avatar1)
+let avatar2 = new Avatar("Yoda", "350")
+console.log(avatar2)
+let avatar3 = new Avatar("Darth Hall", "350")
+console.log(avatar3)
+let avatar4 = new Avatar("Luke Skywalker", "450")
+console.log(avatar4)
+
+let jugador1= new Jugador()
+console.log(jugador1)
+let jugador2= new Jugador()
+console.log(jugador1)
+
+let escenario1= new Escenario()
+
+
+seleccionaJugador = () => {
+
+
+
 
 }
-
-
-let avatar1 = new Jugador(Darth Vader)
-console.log(avatar1)
