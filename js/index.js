af2cf815cb145dcfc60c5089f7fd62544812b860
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-10-15 17:37:42 +0200

    creacion de pantalla2

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
-class Escenario {
-    constructor(seleccion1, seleccion2, ganador) {
+// class Escenario {
+//     constructor(seleccion1, seleccion2, ganador) {
 
-        // this.jugador1 = seleccion1
-        // this.jugador2 = seleccion2
-        // this.ganador = null
-    }
+//         // this.jugador1 = seleccion1
+//         // this.jugador2 = seleccion2
+//         // this.ganador = null
+//     }
 
-
-}
+// }
 
 class Avatar {
     constructor(nombre, fuerza, img) {
         this.nombre = nombre;
         this.fuerza = fuerza
+        this.imagenAvatar = img
     }
 
 }
@@ -23,77 +23,101 @@ class Jugador {
         this.avatar = avatar
     }
 }
-// ganar(vida) {
-//     if (vida.Avatar1.vida == 0) {
-//         alert(`GANASTE+ ${avatar2}`)
-//     } else if () {
 
 
-//     }
-// }
 
-
-let avatar1 = new Avatar("Darth Vader", "200", "/Imagenes/Darth_Vader.jpg")
-console.log(avatar1)
-let avatar2 = new Avatar("Yoda", "350")
-console.log(avatar2)
-let avatar3 = new Avatar("Darth Hall", "350")
-console.log(avatar3)
-let avatar4 = new Avatar("Luke Skywalker", "450")
-console.log(avatar4)
+let avatar1 = new Avatar("Darth Vader", "200",`<img class="imagen-seleccionado" src="/Imagenes/Darth_Vader.jpg"alt="Darth Vader">`)
+// console.log(avatar1)
+let avatar2 = new Avatar("Yoda", "350",`<img class="imagen-seleccionado" src="/Imagenes/Yoda.jpg" alt="Yoda">`)
+// console.log(avatar2)
+let avatar3 = new Avatar("Darth Hall", "350",`<img class="imagen-seleccionado" src="/Imagenes/Darth_hall.jpg"alt="Darth Hall">`)
+// console.log(avatar3)
+let avatar4 = new Avatar("Luke Skywalker", "450",` <img class="imagen-seleccionado" src="/Imagenes/Luke_Skywalker.jpg"alt="-"> `)
+// console.log(avatar4)
 
 let jugador1 = new Jugador()
-console.log(jugador1)
+// console.log(jugador1)
 let jugador2 = new Jugador()
-console.log(jugador2)
-let escenario1 = new Escenario()
+// console.log(jugador2)
+// let escenario1 = new Escenario()
 
 // PROCESO DE SELECCION 
 
-const avatarSelection = (player) => {
+function avatarSelection(player) {
 
     if (jugador1.avatar == undefined || jugador2.avatar == undefined) {
 
-        // alert("hola") //entra en función 
         switch (player) {
-            case 'dartVader':
+
+
+            case 'darthVader':
+
 
                 if (jugador1.avatar == undefined) {
-                    let result = jugador1.avatar = avatar1
+                    jugador1.avatar = avatar1;
+                    alert("selecciona un segundo jugador");
+                } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar1) {
 
+                    jugador2.avatar = avatar1;
+
+                } else {
+
+                    return alert("No puede seleccionar el mismo jugador");
                 }
-                console.log(result) //prueba exitosa
-                break;
+
+
             case 'yoda':
+
                 if (jugador1.avatar == undefined) {
-                    let result = jugador1.avatar = avatar1
+
+                    jugador1.avatar = avatar2;
+
+
+                } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar2) {
+                    jugador2.avatar = avatar2;
 
                 }
-                console.log(result) //prueba
+
             case 'dartHall':
                 if (jugador1.avatar == undefined) {
-                    let result = jugador1.avatar = avatar1
+                    jugador1.avatar = avatar3;
 
+                    alert("selecciona un segundo jugador");
+                } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar3) {
+                    jugador2.avatar = avatar3;
+                    console.log(jugador2);
                 }
-                console.log(result) //prueba
-            case 'lukeSkywalker'
-            if (jugador1.avatar == undefined) {
-                let result = jugador1.avatar = avatar1
+            case 'lukeSkywalker':
+                if (jugador1.avatar == undefined) {
 
-            }
+                    jugador1.avatar = avatar4;
 
+                    alert("selecciona un segundo jugador");
+                } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar4)
+                    jugador2.avatar = avatar4;
 
         }
 
+    } else {
+        starGame(jugador1, jugador2);
+
+    }
+
+
+    const starGame = (a, b) => {
+        console.log(a)
+        console.log(b)
     }
 }
 
 
 
-segundaPantalla()
 
-}
+    const starGame = (a, b) => {
 
+        console.log(a)
+        console.log(b)
 
+    }
 
 
