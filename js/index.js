6cdcb749491820d1ceaa4bf73e6b5b554ca70808
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-10-16 15:36:07 +0200

    modificando la posición de la función starGame: Linea 108 a cada unos de los else if del segundo jugador

--- js/index.js
+++ js/index.js
@@ -10,29 +10,38 @@
 
 class Avatar {
     constructor(nombre, fuerza, img) {
+        this.vida = 100
         this.nombre = nombre;
         this.fuerza = fuerza
         this.imagenAvatar = img
     }
 
+
 }
 class Jugador {
 
     constructor(avatar) {
-        this.vida = 100
         this.avatar = avatar
     }
+    //    atacar(){
+    //        this.vida-this.fuerza
+    //    }
+
+    //    atacar(enemigo) {
+    //        enemigo.vida - this.fuerza
+    //    }
+
 }
 
 
 
-let avatar1 = new Avatar("Darth Vader", "200",`<img class="imagen-seleccionado" src="/Imagenes/Darth_Vader.jpg"alt="Darth Vader">`)
+let avatar1 = new Avatar("Darth Vader", 0, `<img class="imagen-seleccionado" src="/Imagenes/Darth_Vader.jpg"alt="Darth Vader">`)
 // console.log(avatar1)
-let avatar2 = new Avatar("Yoda", "350",`<img class="imagen-seleccionado" src="/Imagenes/Yoda.jpg" alt="Yoda">`)
+let avatar2 = new Avatar("Yoda", 0, `<img class="imagen-seleccionado" src="/Imagenes/Yoda.jpg" alt="Yoda">`)
 // console.log(avatar2)
-let avatar3 = new Avatar("Darth Hall", "350",`<img class="imagen-seleccionado" src="/Imagenes/Darth_hall.jpg"alt="Darth Hall">`)
+let avatar3 = new Avatar("Darth Hall", 0, `<img class="imagen-seleccionado" src="/Imagenes/Darth_hall.jpg"alt="Darth Hall">`)
 // console.log(avatar3)
-let avatar4 = new Avatar("Luke Skywalker", "450",` <img class="imagen-seleccionado" src="/Imagenes/Luke_Skywalker.jpg"alt="-"> `)
+let avatar4 = new Avatar("Luke Skywalker", 0, ` <img class="imagen-seleccionado" src="/Imagenes/Luke_Skywalker.jpg"alt="-"> `)
 // console.log(avatar4)
 
 let jugador1 = new Jugador()
@@ -45,82 +54,99 @@ let jugador2 = new Jugador()
 
 function avatarSelection(player) {
 
+
     if (jugador1.avatar == undefined || jugador2.avatar == undefined) {
+
 
         switch (player) {
 
 
-            case 'darthVader':
-
-
+            case 'darthVader'://avatar1
                 if (jugador1.avatar == undefined) {
+
                     jugador1.avatar = avatar1;
-                    alert("selecciona un segundo jugador");
+
+
                 } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar1) {
 
                     jugador2.avatar = avatar1;
-
-                } else {
-
-                    return alert("No puede seleccionar el mismo jugador");
+                    starGame(jugador1, jugador2);
                 }
-
-
-            case 'yoda':
-
+                break
+            case 'yoda'://avatar2
                 if (jugador1.avatar == undefined) {
 
                     jugador1.avatar = avatar2;
 
-
+                    alert("selecciona un segundo jugador");
                 } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar2) {
                     jugador2.avatar = avatar2;
+                    starGame(jugador1, jugador2);
 
                 }
+                break
 
-            case 'dartHall':
+            case 'dartMaul'://avatar3
                 if (jugador1.avatar == undefined) {
+
                     jugador1.avatar = avatar3;
 
                     alert("selecciona un segundo jugador");
                 } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar3) {
                     jugador2.avatar = avatar3;
-                    console.log(jugador2);
+                    starGame(jugador1, jugador2);
                 }
-            case 'lukeSkywalker':
+                break
+            case 'lukeSkywalker'://avatar4
                 if (jugador1.avatar == undefined) {
 
                     jugador1.avatar = avatar4;
 
+
                     alert("selecciona un segundo jugador");
-                } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar4)
+                } else if (jugador2.avatar == undefined && jugador1.avatar !== avatar4) {
+
                     jugador2.avatar = avatar4;
+                    starGame(jugador1, jugador2);
 
+                }
+                break
         }
-
-    } else {
-        starGame(jugador1, jugador2);
     }
+
 }
+
+
+
 
 let pantallaPrincipalCombate = document.querySelector(".pantalla-principal-combate")
-let pantallaPrincipalSeleccion=document.querySelector(".pantalla-principal-seleccion") 
-let contenedorJugador1=document.querySelector(".contenedor-seleccionado-1")
-let contenedorJugador2=document.querySelector(".contenedor-seleccionado-2")
+let pantallaPrincipalSeleccion = document.querySelector(".pantalla-principal-seleccion")
+let contenedorJugador1 = document.querySelector(".contenedor-seleccionado-1")
+let contenedorJugador2 = document.querySelector(".contenedor-seleccionado-2")
 
 
-
-
-const starGame = (a, b) => {
-    console.log(a)
-    console.log(b)
-    pantallaPrincipalSeleccion.style.display='none'
-    pantallaPrincipalCombate.style.display='flex';
-
-    alert("lo hemos logrado cabron")
+const starGame = (primejugador, segundojugador) => {
+    console.log(primejugador)
+    console.log(segundojugador)
+    pantallaPrincipalSeleccion.style.display = 'none'
+    pantallaPrincipalCombate.style.display = 'flex';
+    contenedorJugador1.innerHTML = jugador1.avatar.imagenAvatar
+    contenedorJugador2.innerHTML = jugador2.avatar.imagenAvatar
 }
 
-funcionAtacar()
+// const atacarContricante1 = () => {
+
+//     restarle la vida el jugador2//
+//     innethtml = ReadableStreamDefaultReader
+
+// }
+// contenedorJugador2 = () => {
+
+// }
 
 
-funcionFinJuego()
+
+// funcionAtacar()
+
+
+// funcionFinJuego()
